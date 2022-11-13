9c927f8eb4d0b26a7bb958a6d0f4859a502ed3cd
burnworks <6436833+burnworks@users.noreply.github.com>
2022-11-13 21:54:46 +0900

    Create api.js

---
+++ blog/src/lib/api.js
@@ -0,0 +1,19 @@
+const ENDPOINT = import.meta.env.SECRET_MT_ENDPOINT_URL;
+
+const fetchAPI = async (path) => {
+    const res = await fetch(`${ENDPOINT}${path}`).catch((err) => { console.error(err) });
+    const json = await res.json();
+    return json;
+}
+
+const fetchAllPosts = () => {
+    const params = [['limit', '5'], ['status', 'Publish']];
+    const param = new URLSearchParams(params).toString();
+
+    return fetchAPI(`?${param}`);
+}
+
+export const getAllPosts = async () => {
+    const data = await fetchAllPosts();
+    return data;
+}
