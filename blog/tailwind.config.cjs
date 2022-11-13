519e4d1cb969c8108c90623ce72d9140c668c5ce
burnworks <6436833+burnworks@users.noreply.github.com>
2022-11-13 23:02:30 +0900

    add custom font setting

--- blog/tailwind.config.cjs
+++ blog/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'body': ['Noto Sans JP', 'Helvetica Neue', 'Arial', 'Hiragino Kaku Gothic ProN', 'Hiragino Sans', 'BIZ UDPGothic', 'Meiryo', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
