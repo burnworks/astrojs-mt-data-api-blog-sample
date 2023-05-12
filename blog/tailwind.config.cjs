0f1c595bda95778f0edb229377e36e7741dcf205
burnworks <6436833+burnworks@users.noreply.github.com>
2023-05-12 18:14:18 +0900

    package update

--- blog/tailwind.config.cjs
+++ blog/tailwind.config.cjs
@@ -2,11 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			fontFamily: {
-				'body': ['Noto Sans JP', 'Helvetica Neue', 'Arial', 'Hiragino Kaku Gothic ProN', 'Hiragino Sans', 'BIZ UDPGothic', 'Meiryo', 'sans-serif'],
-			},
+		fontFamily: {
+			'body': ['"Noto Sans JP"', '"Helvetica Neue"', 'Arial', '"Hiragino Kaku Gothic ProN"', '"Hiragino Sans"', '"BIZ UDPGothic"', 'Meiryo', 'sans-serif'],
 		},
+		extend: {},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
